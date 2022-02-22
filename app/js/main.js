97b22b938e1ff97d11aff1e5ab88e4b1882cc395
KonstantinSin <konstsin0909@gmail.com>
2022-02-22 21:13:17 +0300

    added product list 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,35 @@
 $(function(){
-    $('.top-slider__inner').slick({
-        dots: true,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 2000
+
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });  
+
+    $('.filter-price__input').ionRangeSlider({
+      type: "double",
+      prefix: "$",
+      onStart: function (data){
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to);
+      },
+      onChange: function (data) {
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to);
+    },
     });
+
+
 });
 var mixer = mixitup('.week__content');
 var mixer = mixitup('.design__content');
+
+
+$('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
