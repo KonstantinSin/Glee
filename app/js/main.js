ffabba932ff17bbd3cc61c2a700e976a7f8f1b4a
KonstantinSin <konstsin0909@gmail.com>
2022-03-06 22:02:57 +0300

    added detalis-product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function(){
 
+
+  $('.detalis-one__item-num').styler();
+  $('.detalis-slide__thumb').slick({
+    asNavFor: '.detalis-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+    arrows: false
+    
+  });
+  $('.detalis-slide__big').slick({
+    asNavFor: '.detalis-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
   $(".star").rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
